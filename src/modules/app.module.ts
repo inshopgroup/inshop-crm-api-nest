34e026c1870bff436c46e7a06cf2fb6ae277dc65
Maksym Mukovoz <sgmaxim@gmail.com>
2025-03-19 10:49:59 +0100

    @nestjs/config

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { Index } from '../controllers';
 import { AppService } from '../services/app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [Index],
   providers: [AppService],
 })

1874aecdef3339db07de2f06f4a319c2a35d6a32
Maksym Mukovoz <sgmaxim@gmail.com>
2025-04-17 23:47:15 +0200

    admin routes, api prefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,36 +4,11 @@ import config from './config/config';
 import databaseConfig from './config/database.config';
 import { ClientsModule } from './modules/clients/clients.module';
 import { PermissionsModule } from './modules/permissions/permissions.module';
-import { RouterModule } from '@nestjs/core';
 
 @Module({
   imports: [
     ClientsModule,
     PermissionsModule,
-    RouterModule.register([
-      {
-        path: 'api',
-        module: AppModule,
-        children: [
-          {
-            path: 'clients',
-            module: ClientsModule,
-          },
-          {
-            path: 'permissions',
-            module: PermissionsModule,
-          },
-        ],
-      },
-      // {
-      //   path: 'clients',
-      //   module: ClientsModule,
-      // },
-      // {
-      //   path: 'permissions',
-      //   module: PermissionsModule,
-      // },
-    ]),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig, config],

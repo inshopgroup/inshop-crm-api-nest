f610f063fb0c57d7f74937324c7408fcd712ac5d
Maksym Mukovoz <sgmaxim@gmail.com>
2025-04-17 20:40:07 +0200

    Configuration

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { Index } from '../controllers';
 import { AppService } from '../services/app.service';
 import { ConfigModule } from '@nestjs/config';
+import config from '../config/config';
+import databaseConfig from '../config/database.config';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, config],
+    }),
+  ],
   controllers: [Index],
   providers: [AppService],
 })

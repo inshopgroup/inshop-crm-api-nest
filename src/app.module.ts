dbb1b17439baa436c8057539bb4887906a7c6b8c
Maksym Mukovoz <sgmaxim@gmail.com>
2025-04-26 15:12:42 +0200

    configure database connection and add docker support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import config from './config/config';
-import databaseConfig from './config/database.config';
 import { ClientsModule } from './modules/clients/clients.module';
 import { PermissionsModule } from './modules/permissions/permissions.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'node:process';
 
 @Module({
   imports: [
@@ -11,7 +12,17 @@ import { PermissionsModule } from './modules/permissions/permissions.module';
     PermissionsModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [databaseConfig, config],
+      load: [config],
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql' | 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +(process.env.DATABASE_PORT as string),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: !!process.env.DATABASE_SYNCHRONIZE,
     }),
   ],
   controllers: [],
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,6 +1,11 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
+  type: process.env.DATABASE_TYPE,
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
+  name: process.env.DATABASE_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  synchronize: process.env.DATABASE_SYNCHRONIZE,
 }));
